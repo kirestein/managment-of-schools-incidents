1069db708be0d503991f7cce1d59601d2304c438
kirestein <erik.proenca2011@gmail.com>
2024-06-18 12:20:01 -0300

    [update] change title, links routes and footer

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -11,28 +11,35 @@ import {
 import Logo from '../../assets/logo.svg'
 import { PiSealWarningFill } from "react-icons/pi";
 import { FaBell } from "react-icons/fa";
+import { obterWeather } from '../../DataBase/db';
 
 export default class Aside extends Component {
+  componentDidMount() {
+    let weatherElement: any = document.getElementById('weather')
+    obterWeather().then(dados => {
+      weatherElement.textContent = dados;
+    });
+  }
   render() {
     return (
       <Container>
         <Header>
           <LogImg src={Logo} alt="logo" />
-          <Title>Meu App</Title>
+          <Title>Gestão de Ocorrências</Title>
         </Header>
 
         <MenuContainer>
-          <MenuItemLink href="#">
+          <MenuItemLink href="/ocorrencias">
             <FaBell />
             Ocorrências
           </MenuItemLink>
-          <MenuItemLink href="#">
+          <MenuItemLink href="/alunos-advertidos">
             <PiSealWarningFill />
             Alunos Advertidos
           </MenuItemLink>
         </MenuContainer>
         <Footer>
-          Aqui é a parte debaixo
+          <span id="weather"></span>
         </Footer>
       </Container>
     )
