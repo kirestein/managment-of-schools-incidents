dd4031a4daae74934c80bfc2ca35deaec8040773
kirestein <erik.proenca2011@gmail.com>
2024-06-18 12:20:50 -0300

    [update] add the scroll vertical bar

--- src/components/Content/style.ts
+++ src/components/Content/style.ts
@@ -5,4 +5,7 @@ export const Container = styled.div`
     color: ${props => props.theme.colors.white};
     background-color: ${props => props.theme.colors.primary};
     padding: 1.5rem;
+
+    height: calc(100vh - 70px);
+    overflow-y: scroll;
 `
