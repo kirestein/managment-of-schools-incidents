839431f633e27b375e8acd4d836fc7643fae1b29
kirestein <erik.proenca2011@gmail.com>
2024-06-18 12:29:35 -0300

    [organize] removed space

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -7,7 +7,7 @@ export default {
         white: '#fff',
         black: '#000',
         gray: '#bfbfbf',
-        success: '#4e41f0 ',
+        success: '#4e41f0',
         info: '#f7931b',
         warning: '#e44c4e'
     },
